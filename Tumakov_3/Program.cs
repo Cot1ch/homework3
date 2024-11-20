79b0819521c7ff908a8eea0a231915f5152f49bf
Cot1ch <vsevolod310306@mail.ru>
2024-11-20 18:39:51 +0300

    Добавьте файлы проекта.

---
+++ Tumakov_3/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Net;
+
+
+namespace Tumakov3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task1();
+            Task2();
+            Task3();
+
+            Console.WriteLine("Введите что-нибудь, чтобы закрыть консоль");
+            Console.ReadKey();
+        }
+        static void Day(int day, bool leapYear = false)
+        {
+            ///summary
+            ///Метод принимает номер дня и возврвщает число. Обработка исключений производится непосредственно во втором задании и на всякий случай здесь
+            ///summary
+
+            int month = -1;
+            int[] monthsDays = { 31, leapYear == true ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            for (int monthCount = 0; monthCount < 12; monthCount++)
+            {
+                if (day - monthsDays[monthCount] > 0)
+                {
+                    day -= monthsDays[monthCount];
+                }
+                else
+                {
+                    month = monthCount;
+                    break;
+                }
+            }
+            switch (month + 1)
+            {
+                case 1:
+                    Console.WriteLine($"Это {day} января");
+                    break;
+                case 2:
+                    Console.WriteLine($"Это {day} февраля");
+                    break;
+                case 3:
+                    Console.WriteLine($"Это {day} марта");
+                    break;
+                case 4:
+                    Console.WriteLine($"Это {day} апреля");
+                    break;
+                case 5:
+                    Console.WriteLine($"Это {day} мая");
+                    break;
+                case 6:
+                    Console.WriteLine($"Это {day} июня");
+                    break;
+                case 7:
+                    Console.WriteLine($"Это {day} июля");
+                    break;
+                case 8:
+                    Console.WriteLine($"Это {day} августа");
+                    break;
+                case 9:
+                    Console.WriteLine($"Это {day} сентября");
+                    break;
+                case 10:
+                    Console.WriteLine($"Это {day} октября");
+                    break;
+                case 11:
+                    Console.WriteLine($"Это {day} ноября");
+                    break;
+                case 12:
+                    Console.WriteLine($"Это {day} декабря");
+                    break;
+                default:
+                    Console.WriteLine("Нет такого дня");
+                    break;
+            }
+        }
+        static void Task1()
+        {
+
+            //
+            //
+            //
+            Console.WriteLine("Упражнение 4.1\n");
+            Console.WriteLine("Введите целое число от 1 до 365 - номер дня в году");
+            int dayNum1;
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int res))
+                {
+                    Console.WriteLine("А если прочитать ТЗ?) Снова пробуем");
+                }
+                else
+                {
+                    dayNum1 = res;
+                    break;
+                }
+            }
+            Day(dayNum1);
+        }
+        static void Task2()
+        {
+            //
+            //
+            //
+            Console.WriteLine("Упражнение 4.2\n");
+            Console.WriteLine("Введите целое число от 1 до 365 - номер дня в году");
+            int dayNum2;
+
+            while (true)
+            {
+                if (!(int.TryParse(Console.ReadLine(), out int res) & 1 <= res & res <= 365))
+                {
+                    Console.WriteLine("А если прочитать ТЗ?) Целое от 1 до 365 включительно. Снова");
+                }
+                else
+                {
+                    dayNum2 = res;
+                    break;
+                }
+            }
+            Day(dayNum2);
+        }
+        static void Task3()
+        {
+            //
+            //
+            //
+            Console.WriteLine("Упражнение 4.2\n");
+            Console.WriteLine("Введите год");
+            int year;
+            while (true)
+            {
+                if (!(int.TryParse(Console.ReadLine(), out int resYear) & 0 <= resYear))
+                {
+                    Console.WriteLine("Я понимаю, что технически такой год был/будет. Но можно в реальность вернуться? Спасибо. Заново");
+                }
+                else
+                {
+                    year = resYear;
+                    break;
+                }
+            }
+            bool isLeap;
+            if (year % 400 == 0)
+            {
+                isLeap = true;
+                Console.WriteLine("Год високосный");
+            }
+            else if (year % 100 == 0)
+            {
+                isLeap = false;
+                Console.WriteLine("Год невисокосный");
+            }
+            else if (year % 4 == 0)
+            {
+                isLeap = true;
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                isLeap = false;
+                Console.WriteLine("Год невисокосный");
+            }
+            Console.WriteLine($"Введите номер дня в году");
+            int dayNum3;
+
+            while (true)
+            {
+                if (!(int.TryParse(Console.ReadLine(), out int res) & 1 <= res & (res <= 366 & isLeap) || (res <= 365 & !isLeap)))
+                {
+                    Console.WriteLine("А если прочитать ТЗ?). Лэт'с гоу эгэйн");
+                }
+                else
+                {
+                    dayNum3 = res;
+                    break;
+                }
+            }
+            Day(dayNum3, isLeap);
+        }
+    }
+}
---
+++ homework3/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+
+namespace homework3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+
+            Console.WriteLine("Нажмите что-нибудь для выхода из этого кринжа");
+            Console.ReadKey();
+        }
+        static double[] EnterArray()
+        {
+            string[] array;
+            double[] nums = new double[10];
+            bool flag = true;
+
+            do
+            {
+                Console.WriteLine("Введите через пробел последовательность из 10 чисел");
+                array = Console.ReadLine().Split();
+                if (array.Length != 10)
+                {
+                    Console.WriteLine("Нужен массив из 10 чисел");
+                }
+                else
+                {
+                    bool flag2 = true;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (!double.TryParse(array[i], out double num))
+                        {
+                            Console.WriteLine($"Ошибка в числе {array[i]} - оно не число:)");
+                            flag2 = false;
+                            break;
+                        }
+                        else
+                        {
+                            nums[i] = num;
+                        }
+                    }
+                    if (flag2)
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            while (flag);
+            return nums;
+        }
+
+        static void Task1()
+        {
+            //
+            //
+            //
+            Console.WriteLine("1 задание\n");
+
+            double[] nums = EnterArray();
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (nums[i] >= nums[i + 1])
+                {
+                    Console.WriteLine($"На {i + 1} элементе возрастание последовательности нарушается");
+                    return;
+                }
+            }
+            Console.WriteLine("Последовательность упорядочена по возрастанию");
+        }
+
+        static void Task2()
+        {
+            //
+            //
+            //
+            Console.WriteLine("2 задание\n");
+            try
+            {
+                Console.WriteLine("Введите номер карты - число от 6 до 14");
+
+                int cardNum = int.Parse(Console.ReadLine());
+                while (6 > cardNum || 14 < cardNum)
+                {
+                    Console.WriteLine("От 6 до 14 включительно!!!!!!!!\nВведите вновь");
+                    cardNum = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Эта карта - {(Cards)cardNum}!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Это не целое число!");
+            }
+            finally
+            {
+                Console.WriteLine("Спасибо за уделённое время");
+            }
+
+        }
+        static void Task3()
+        {
+            //
+            //
+            //
+            Console.WriteLine("3 задание\n");
+
+            Console.WriteLine("Скажи мне, кто ты, и я скажу, чем садить печень");
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+        }
+        static void Task4()
+        {
+            //
+            //
+            //
+            Console.WriteLine("4 задание\n");
+
+            Console.WriteLine("Введите порядковый номер дня недели (1 - 7)");
+            string smth;
+            while (true)
+            {
+                smth = Console.ReadLine();
+                if (!byte.TryParse(smth, out byte dayNumber))
+                {
+                    Console.WriteLine("Ну такого дня точно нет, давай по новой");
+                }
+                else if (1 > dayNumber || 7 < dayNumber)
+                {
+                    Console.WriteLine("Нет такого дня недели, заново давай");
+                }
+                else
+                {
+                    Console.WriteLine($"{dayNumber} день недели - {(Week)dayNumber}");
+                    break;
+                }
+            }
+
+        }
+        static void Task5()
+        {
+            //
+            //
+            //
+            Console.WriteLine("5 задание\n");
+
+            // string[] dolls = {"Hello Kitty", "Teddy Bear", "Mr. Monopoly", "Hello Kitty", "Barbie doll"}
+            Console.WriteLine("Введите в одной строке через нижнее подчёркивание игрушки");
+            Console.WriteLine("Пр.: Hello Kitty_Mr. Monopoly");
+
+            int bag = 0;
+            foreach (string doll in Console.ReadLine().Split('_')) //Либо foreach (string doll in dolls)
+            {
+                if (doll.ToLower() == "hello kitty" || doll.ToLower() == "barbie doll")
+                {
+                    bag++;
+                }
+            }
+            Console.WriteLine($"Кукол в сумке = {bag}");
+        }
+        public enum Cards
+        {
+            Шестёрка = 6,
+            Семёрка = 7,
+            Восьмёрка = 8,
+            Девяточка = 9,
+            Десятка = 10,
+            Валет = 11,
+            Дама = 12,
+            Король = 13,
+            Туз = 14
+        }
+        public enum Week
+        {
+            понедельник = 1,
+            вторник,
+            среда,
+            четверг,
+            пятница,
+            суббота,
+            воскресенье
+        }
+    }
+}
